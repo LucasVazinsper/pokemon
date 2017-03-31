32cce46f2dbaa2af142904bd8da1e371c3e406df
Juan Jorge Garcia <juan.j.garcia007@gmail.com>
2017-03-31 09:49:40 -0300

    uou

---
+++ kk.py
@@ -0,0 +1,10 @@
+n=int(input("insira um numero positivo: "))
+while n>0:
+	y=int(input("insira outra numero positivo: "))
+	a=n+y
+	print(a)
+	k=int(input("insira outro numero:  "))
+	print(K+a)
+	
+	
+print("nao gostamos de numeros negativos")	
