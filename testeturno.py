45180db7aaab6beaef2bf1ace805eacba2e2082f
Juan Jorge Garcia <juan.j.garcia007@gmail.com>
2017-04-04 18:48:36 -0300

    Comando invalido concertado

--- testeturno.py
+++ testeturno.py
@@ -17,16 +17,16 @@ inimigo_defesa=0
 inimigo_speed=1
 ok=True
 while ok:
-    a=int(input("passear (0) ou dormir (1): "))
-    if a==1:
+    a=input("passear (0) ou dormir (1): ")
+    if a=="1":
         break
-    if a==0:
+    if a=="0":
         while minha_vida>0 and inimigo_vida>0:
-            acao=int(input("atacar:0 correr:1    "))
-            if acao==1:
+            acao=input("atacar:0 correr:1    ")
+            if acao=="1":
                 delay_print("Voce correu.")
                 break
-            if acao==0 and minha_speed>inimigo_speed:
+            elif acao=="0" and minha_speed>inimigo_speed:
                 inimigo_vida=inimigo_vida+inimigo_defesa-meu_ataque
                 delay_print("sua vida:{} vida do inimigo:{}\n".format(minha_vida,inimigo_vida))
                 if minha_vida>0 and inimigo_vida>0:
@@ -36,7 +36,7 @@ while ok:
                     delay_print("Voce venceu!!\n")
                 elif minha_vida<=0:
                     delay_print("Vc perdeu...\n")
-            if acao==0 and minha_speed<inimigo_speed:
+            elif acao=="0" and minha_speed<inimigo_speed:
                 minha_vida=minha_vida+minha_defesa-inimigo_ataque
                 delay_print("sua vida:{} vida do inimigo:{}\n".format(minha_vida,inimigo_vida))
                 if minha_vida>0 and inimigo_vida>0:
@@ -46,7 +46,7 @@ while ok:
                     print("Voce venceu!!\n")
                 elif minha_vida<=0:
                     delay_print("Vc perdeu...\n")
-            elif acao!=0 and acao!=1:
+            else:
                 print("Digite um comando válido ")  #caso o usuario insira um numero errado
     else:
-        print("Digite um comando válido 5")
+        print("Digite um comando válido ")
