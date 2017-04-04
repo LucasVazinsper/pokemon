521bf60c1a41443dc8fe8f83eba5da77ef85b113
Juan Jorge Garcia <juan.j.garcia007@gmail.com>
2017-04-04 17:28:40 -0300

    Mando muito mister n faz nada olha essa classe

---
+++ classe.py
@@ -0,0 +1,16 @@
+Tipos=["Normal","Fire","Water","Eletric","Grass","Ice","Fighting","Poison","Ground","Flying","Psychic","Bug","Ghost","Rock","Dragon"]
+
+class Pokemon:
+	#classe de atributos para pokemons
+
+	def __init__(self,vtipo,vhp,vatk,vdeff,vspd):
+		self.hp=vhp  #vida do pokemon
+		self.deff=vdeff  # defesa do pokemon
+		self.atk=vatk  # ataque do pokemon
+		self.spd=vspd  # velocidade do pokemon
+		self.tipo=vtipo  #tipo do pokemon
+		self.atributos=print("Type:{}\nHp:{}\nAttack:{}\nDeffense:{}\nSpeed:{}\n".format(self.tipo,self.hp,self.atk,self.deff,self.spd))
+
+Charmander = Pokemon(Tipos[1],39,52,43,65)
+Squirtle = Pokemon(Tipos[2],44,48,65,43)
+print(Charmander.atributos,Squirtle.atributos)
