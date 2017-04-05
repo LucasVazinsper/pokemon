bb667d212afcb86e1f4beda35fe85fc84014c722
LucasVazinsper <lucastv.al.insper.edu.br>
2017-04-05 15:49:45 -0300

    melhores equacoes

--- damage funcao.py
+++ damage funcao.py
@@ -1,13 +1,8 @@
+import math
 def dano(IVatk,baseatk,mov,levelA,IVhp,IVdef,basedef,basehp,levelB):
-    ev=22850
-    evraiz=(math.sqrt(ev))/4
-    ran=90
-    hpb=(((((basehp+IVhp)*2)+(evraiz))*levelB)/100)+levelB+10
-    atka=(((((baseatk+IVatk)*2)+(evraiz))*levelA)/100)+5
-    defb=(((((basedef+IVdef)*2)+(evraiz))*levelB)/100)+5
+    ev=(100) #valor de 1 ate 254
+    hpb=(((2*basehp+IVhp+(ev/4))*levelB)/100)+levelB+10
+    atka=(((2*baseatk+IVatk+(ev/4))*levelA)/100)+5
+    defb=(((2*basedef+IVdef+(ev/4))*levelB)/100)+5
     dano=(((((2*levelA)/5)+2)*mov)*(atka/defb)/50)+2    #nao esta certa...
-    return dano
-
-hp=dano(30,52,110,5,30,30,65,44,5)
-print (int(hp))
-    
+    return hpb
