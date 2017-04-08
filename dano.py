f2a84bda59749f5844ea679173812536e18953c0
LucasVazinsper <lucastv.al.insper.edu.br>
2017-04-07 21:54:20 -0300

    funcao de dano

---
+++ dano.py
@@ -0,0 +1,5 @@
+def dano(atk,lvl,move,deff):
+    ran=random.randrange(85,100)
+    x=1 #modificador(super efetivo)
+    damage=((((2*lvl/5)+2)*move*(atk/deff)/50)+2)*(ran/100)*(x)
+    return damage
