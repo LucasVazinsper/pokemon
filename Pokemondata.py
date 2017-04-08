1804a0cb7d114783bcb423bda499d9da3ff0b212
LucasVazinsper <lucastv.al.insper.edu.br>
2017-04-07 22:07:51 -0300

    teste

--- Pokemondata.py
+++ Pokemondata.py
@@ -2,20 +2,21 @@ pok={"bulbasaur":{"type":"grass",
 					"hp":45,
 					"atk":49,
 					"deff":49,
-					"spd"40,
-					"satk":40,},
+					"spd":40,
+					"satk":40},
 	"charmander":{"type":"fire",
 					"hp":49,
 					"atk":52,
 					"deff":43,
 					"spd":65,
-					"satk":40,}
+					"satk":40},
 	"squirtle":{"type":"water",
 	              "hp":44,
                   "atk":50,
-				  "deff"65,
+				  "deff":65,
 				  "spd":43,
-				  "satk":40,}
+				  "satk":40}}
+
 
 
 
