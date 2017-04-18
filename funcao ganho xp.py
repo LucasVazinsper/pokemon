b96364e78325619b36357b8d554507cc5e6b24f6
LucasVazinsper <lucastv.al.insper.edu.br>
2017-04-17 22:15:53 -0300

    funcao para o ganho de exp

---
+++ funcao ganho xp.py
@@ -0,0 +1,5 @@
+def xpgamho(L,xp):
+    return 1.5*xp*1*1*1.5*L/7
+    #xp=o tanto que cada pokemon da
+    #L=lvl do pokemon derrotado
+    #os numeros fixos sao valores que nao podem ser levados em consideracao vatiavel dentro do nosso jogo
